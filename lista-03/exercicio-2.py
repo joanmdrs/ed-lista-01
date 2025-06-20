18173d76fa292fc3ebac132b64af80c1f530cc52
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-06-20 16:07:39 -0300

    adicionando comentários ao código fonte

--- lista-03/exercicio-2.py
+++ lista-03/exercicio-2.py
@@ -1,3 +1,15 @@
+""" 
+Este programa recebe como entrada uma lista de números. 
+
+Após isso, é criado o conjunto (alunos_presentes) que será utilizado para armazenar os números dos alunos presentes.
+
+Para preencher o conjunto de alunos_presentes, foi usado um loop for que itera sobre a lista de entrada. A cada iteração, um elemento é adicionado ao conjunto. 
+
+Como a estrutura de conjunto não permite elementos repetidos, o mesmo irá realizar a inserção de números que já existem no conjunto. 
+
+Por fim, é exibido a quantidade de alunos presentes.  
+"""
+
 lista_registro = [3,2,3,1]
 
 alunos_presentes = set()
--- lista-03/exercicio-3.py
+++ lista-03/exercicio-3.py
@@ -1,3 +1,15 @@
+"""
+Este programa recebe como entrada uma lista de números, representando registros de presença de alunos.
+
+Em seguida, é criado o conjunto alunos_presentes, que será utilizado para armazenar os números dos alunos presentes de forma única.
+
+Para preencher esse conjunto, é utilizado um loop for que percorre a lista de registros. A cada iteração, o número do aluno é adicionado ao conjunto.
+
+Como conjuntos não permitem elementos duplicados, qualquer número repetido na lista será automaticamente ignorado ao ser inserido.
+
+Ao final, é exibida a quantidade total de alunos presentes, ou seja, a quantidade de elementos únicos no conjunto alunos_presentes.
+"""
+
 lista_registro = [1, 0, 5, 6, 0, 12, 25, 6, 2, 6, 5, 0, 25, 13, 2]
 
 registro_unico = set()
