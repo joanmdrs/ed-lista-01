50a9021de109cd8cd762c0acde3e3e203ab31fc6
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-06-22 14:05:06 -0300

    feat: implementa a continuação do exercício 1.25

--- lista-04/exercicio-1.25.py
+++ lista-04/exercicio-1.25.py
@@ -29,3 +29,43 @@ def calcular_fatorial(n):
 numero = int(input("Informe o número que deseja calcular o fatorial: "))
 resultado = calcular_fatorial(numero)
 print(f"O fatorial de {numero} é: {resultado}")
+
+"""
+A sequência de Fibonacci é uma sequência numérica, onde cada termo, a partir do terceiro, é a soma dos dois termos anteriores. 
+
+Sendo assim, a função fibonacci recebe como parâmetro um número n. 
+ 
+Se n = 1, o programa retorna 0 (caso trivial onde sabemos o resultado).
+Se n = 2, o programa retorna 1 (caso trivial onde sabemos o resultado).
+
+Se não, o programa realiza duas chamadas recursivas, calculando o valor das posições (n - 1) e (n - 2), e retorna a soma desses dois valores.
+
+Isso gera um efeito de empilhamento de chamadas recursivas, até que se alcancem os casos triviais. A partir daí, os valores começam a ser somados e retornados para as chamadas anteriores, até que se obtenha o valor final da posição n.
+
+Por exemplo:
+fibonacci(5)
+= fibonacci(4) + fibonacci(3)
+= (fibonacci(3) + fibonacci(2)) + (fibonacci(2) + fibonacci(1))
+= ((fibonacci(2) + fibonacci(1)) + 1) + (1 + 0)
+= ((1 + 0) + 1) + (1 + 0)
+= (1 + 1) + 1
+= 2 + 1
+= 3
+
+Portanto, o valor da 5ª posição da sequência de Fibonacci (considerando n = 1 como a primeira posição) é 3.
+"""
+
+def fibonacci(n):
+    
+    if n == 1:
+        return 0
+    
+    elif n == 2:
+        return 1
+    
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+numero = int(input("Informe a posição que deseja saber o valor na sequência de Fibonacci: "))
+resultado = fibonacci(numero)
+print(f"O valor da posição {numero}º na sequência de Fibonacci é: {resultado}")
